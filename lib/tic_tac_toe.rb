2611cd09af9fb04af9e9ee7214f1bcdbdc6b55b0
Ethan <ethansagin@gmail.com>
2019-02-27 21:25:51 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -23,7 +23,7 @@ def input_to_index(user_input)
 end
 
 def move(board, index, current_player)
-  board[index] = current_player(board)
+  board[index] = !current_player(board)
 end
 
 def position_taken?(board, location)
